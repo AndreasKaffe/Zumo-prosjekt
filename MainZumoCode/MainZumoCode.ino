0c18ea9c16dd5b51c484829334cea07c7918a3fd
Toothyarmor <sandersb@stud.ntnu.no>
2021-05-04 19:04:57 +0200

    code with sensors

--- MainZumoCode/MainZumoCode.ino
+++ MainZumoCode/MainZumoCode.ino
@@ -2,28 +2,37 @@
 
 //pins
 const int LED = 13;
+const int EchoPin1 = 6;
+const int TrigPin1 = 2;
+const int EchoPin2 = A1;
+const int TrigPin2 = A0;
+
 
 //states
-const int S_CountDown = 0;
-const int S_Drive = 1;
-const int S_Ram = 2;
-const int S_Edge = 3;
-const int S_dodge = 4;
+
+const int S_RushDown = 0;
+const int S_Search = 1;
+
+
+
 
 //const values
 const int fiveSeconds = 5000;
-const int oneFifth = 200;
-const int threeTenths = 300;
+const int delay1 = 500;
+const int delay2 = 500;
 
 const int speedHigh = 350;
-const int speedMid = 200;
+const int speedMid = 150;
 const int speedLow = 100;
 const int threshold = 1500;
 
 //used variables
 unsigned long nextTimeout;
-int currentState = S_CountDown;
-
+int currentState = S_Search;
+int rightDistance = 200;
+int leftDistance = 200;
+int right;
+int left;
 //Zumostuff
 ZumoBuzzer buzzer;
 ZumoMotors motors;
@@ -38,19 +47,52 @@ ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN);
 void setup()
 {// put your setup code here, to run once:
   pinMode(LED, HIGH);
-  
-  
+  pinMode(TrigPin1, OUTPUT);
+  pinMode(EchoPin1, INPUT);
   countDown();
 }
 
 void loop()
 {
-  sensors.read(sensor_values);
-
+  switch (currentState)
+  {
+    case(S_RushDown):
+    
+    right = speedLow;
+    left = speedLow;
+    leftDistance = LydDistance(TrigPin1, EchoPin1);
+    rightDistance = LydDistance(TrigPin2, EchoPin2);
+    if (leftDistance <= 100)
+    {
+      right = speedMid;
+    
+    }
+    if (rightDistance <= 100)
+    {
+      left = speedMid;
+      
+    }
+    motors.setSpeeds(right, left);
+    if ((right == speedLow)&&(right == speedLow))
+    {
+      motors.setSpeeds(-speedLow, speedLow);
+      currentState = S_Search;
+    }
+    break;
+      
+    case(S_Search):
+    motors.setSpeeds(-speedLow,speedLow); //rotate on the spot
+    leftDistance = LydDistance(TrigPin1,EchoPin1);
+    rightDistance = LydDistance(TrigPin2, EchoPin2);
+    if (((leftDistance) <= 100)||(rightDistance <= 100)) //oppdager et objekt innen 100 cm
+    {
+      motors.setSpeeds(speedMid, speedMid); //drive forward (should at triangulation function)
+      currentState = S_RushDown;
+    }
+    break;
+  }
   turnAtEdge();
-
-  motors.setSpeeds(speedMid, speedMid);
-  
+  StoppButton();
 }
 
 void countDown()
@@ -58,45 +100,28 @@ void countDown()
   digitalWrite(LED, HIGH);
   button.waitForButton();
   digitalWrite(LED, LOW);
-
   delay(5000);
-}
-
-void timer(int wait)
-{
-  unsigned long currentTime = millis();
-  nextTimeout = currentTime + wait;
-}
-bool timeExpired()
-{
-  unsigned long currentTime = millis();
-  if(currentTime < nextTimeout)
-  {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
 }
 
 void turnAtEdge()
 {
+  sensors.read(sensor_values);
+
   
-  if (sensor_values[0] > threshold)
+  if (sensor_values[5] < threshold)
   {
     motors.setSpeeds(-speedMid, -speedMid);
-    delay(oneFifth);
+    delay(delay1);
     motors.setSpeeds(-speedMid, speedMid);
-    delay(threeTenths);
+    delay(delay2);
     motors.setSpeeds(speedMid, speedMid);
   }
-  else if (sensor_values[5] > threshold)
+  else if (sensor_values[0] < threshold)
   {
     motors.setSpeeds(-speedMid, -speedMid);
-    delay(oneFifth);
+    delay(delay1);
     motors.setSpeeds(speedMid, -speedMid);
-    delay(threeTenths);
+    delay(delay2);
     motors.setSpeeds(speedMid, speedMid);
   }
 }
@@ -110,6 +135,18 @@ void StoppButton()
     button.waitForRelease();
     countDown();
   }
+}
+
+int LydDistance(int Trig, int Echo)
+{
+  digitalWrite(Trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(Trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Trig, LOW);
+  long duration = pulseIn(Echo, HIGH);
+  int distance = (duration * 0.034 / 2);
+  return distance;
 }
 /*
  timer();// put your main code here, to run repeatedly:
